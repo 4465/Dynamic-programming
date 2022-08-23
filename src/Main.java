780ba057649300864304d04078403a2969e1026d
4465 <lwfrekach@gmail.com>
2022-08-23 10:45:07 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author 刘威甫
+ * @date 2022/8/23 10:42
+ * @description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("动态规划练习开始");
+    }
+}
