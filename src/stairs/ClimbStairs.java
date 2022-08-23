bf04f7563b8744a061e6c85ac32e44c254a7229c
4465 <lwfrekach@gmail.com>
2022-08-23 11:18:04 +0800

    爬楼梯和README

---
+++ src/stairs/ClimbStairs.java
@@ -0,0 +1,35 @@
+package stairs;
+
+import java.util.*;
+
+/**
+ * @author 刘威甫
+ * @date 2022/8/23 11:03
+ * @description
+ */
+public class ClimbStairs {
+
+
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 6; i++) {
+            int n = sc.nextInt();
+            ClimbStairs climbStairs = new ClimbStairs();
+            int ans = climbStairs.climbStairs(n);
+            System.out.println(ans);
+        }
+
+    }
+
+}
